f5345eed38141549f8c861290770d1eddbfbc133
viniciusagvaz <agvazvinicius@gmail.com>
2023-10-09 15:45:47 -0300

    componente header finalizado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+
+import logo  from "../../assets/logo.svg"
+
+export function Header() {
+  return(
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logo} alt="" />
+        <NewTransactionButton>
+          Nova Transação
+        </NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}

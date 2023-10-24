893277149357f529ca5f9760f70be5032ff15b21
viniciusagvaz <agvazvinicius@gmail.com>
2023-10-24 17:18:14 -0300

    Modal de nova transação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from "@radix-ui/react-dialog";
 
 import logo  from "../../assets/logo.svg"
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return(
@@ -12,20 +13,9 @@ export function Header() {
         <Dialog.Root>
           <Dialog.Trigger asChild>
             <NewTransactionButton> Nova Transação </NewTransactionButton>
+
           </Dialog.Trigger>
-
-          <Dialog.Portal>
-            <Dialog.Overlay />
-
-            <Dialog.Content>
-              <Dialog.Title>
-                Nova Transação
-              </Dialog.Title>
-            
-            <Dialog.Close />
-            
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal/>
         </Dialog.Root>
       
       </HeaderContent>

d8f3b518205cfc1531d995388fab129f5dab829b
viniciusagvaz <agvazvinicius@gmail.com>
2023-10-24 15:39:28 -0300

    Componente: Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
+import { SummaryContainer, SummaryCard } from "./styles";
+
+export function Summary(){
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e"/>
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68"/>
+        </header>
+        <strong>R$ 8.200,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Entradas</span>
+          <CurrencyDollar size={32} color="#fff"/>
+        </header>
+        <strong>R$ 9.200,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}

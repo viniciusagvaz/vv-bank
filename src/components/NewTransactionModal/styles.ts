9e13c8ec84ad40c802dfba75abfc605fed06776f
viniciusagvaz <agvazvinicius@gmail.com>
2023-10-26 18:23:53 -0300

    Aplicando React Hook Form

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -49,7 +49,12 @@ export const Content = styled(Dialog.Content)`
       margin-top: 1.5rem;
       cursor: pointer;
 
-      &:hover{
+      &:disabled{
+        opacity: 0.5;
+        cursor: not-allowed;
+      }
+
+      &:not(:disabled):hover{
         background: ${props => props.theme['green-700']};
         transition: background-color .2s;
       }
